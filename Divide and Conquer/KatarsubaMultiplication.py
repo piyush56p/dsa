32ef473ad1d311fbc3a4dbfb10def96b408985a1
Piyush Sharma <70280797+piyush56p@users.noreply.github.com>
2022-07-12 21:46:32 +0530

    O(n^1.58)

---
+++ Divide and Conquer/KatarsubaMultiplication.py
@@ -0,0 +1,19 @@
+def karatsuba(x,y):
+    if (len(str((x))) == 1 or len(str((y))) == 1):
+        return x*y
+    else:
+        n = max(len(str((x))), len(str((y))))
+        nby2 = n//2
+        a = x//(10**(nby2))
+        b = x%(10**(nby2))
+        c = y//(10**(nby2))
+        d = y%(10**(nby2))
+
+        ac = karatsuba(a,c)
+        bd = karatsuba(b,d)
+        ad_plus_bc = karatsuba(a+b,c+d) -ac -bd
+        return ac*10**(2*nby2) + ad_plus_bc*10**(nby2) + bd
+x = 146123
+y = 352120
+pro = karatsuba(x,y)
+print(pro)
